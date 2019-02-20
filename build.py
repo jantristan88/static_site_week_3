b8d3e77420a4240afc9c6759c0914bb274975dc2
Jan Tristan <jan.tristan@gmail.com>
2019-02-19 21:59:32 -0800

    Added the title function and added comments

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 def main():
 
+	#Created a list of dictionaries of the files involved, the directory to which the output should go
+	#and the title which will eventually be used for the placeholder
 	pages = [
 		{
 			"filename":"content/index.html",
@@ -19,14 +21,25 @@ def main():
 
 		] 
 
-	def combine()
+	#Used a for loop for the following commands be applied to each dictionary set
 	for page in pages:
-		template = open("templates/base.html").read()
-		index_content = open(page['filename']).read()
-		finished_index_page = template.replace("{{content}}", index_content)
-		open(page['output'], "w+").write(finished_index_page)
+		template = open("templates/base.html").read() #Opens and assigns base.html in the templates folder to variable template
+		index_content = open(page['filename']).read() #Opens and assigns the values of the key filename to index_content
+		finished_index_page = template.replace("{{content}}", index_content) #opens the the variable template, replaces the placeholder content with the variable index_content
+		open(page['output'], "w+").write(finished_index_page) #the values assigned to variable finished_index_page is written to the values in the output key
+															  #modifying or creating the html giles in the docs folder
+
+	#created another function to address the title placeholders													
+	def title():
+		for page in pages:
+			title_content = open(page['filename']).read()
+			title_name = page['title']
+			finished_title_content = title_content.replace("{{title}}", title_name)
+			open(page['output'], "w+").write(finished_title_content)
+	title()
+
 
 if __name__ == '__main__':
-	main()
+	main() #invokes the main function
 
 
